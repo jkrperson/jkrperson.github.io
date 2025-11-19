b8c3014a49e4f25a5675131f89f583e5fc476870
jkrperson <josekristianresabal@gmail.com>
2025-11-19 16:31:11 +0800

    Remove Astro site and base configuration and delete GitHub Pages deployment workflow.

--- astro.config.mjs
+++ astro.config.mjs
@@ -6,8 +6,6 @@ import tailwindcss from '@tailwindcss/vite';
 
 // https://astro.build/config
 export default defineConfig({
-  site: 'https://jkresabal.github.io',
-  base: '/jkrperson.github.io',
   integrations: [svelte()],
 
   vite: {
